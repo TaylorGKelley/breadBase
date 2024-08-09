a43800383f9766dd0e8a6a9992bdf5d129d55028
Taylor Kelley <taylorgkelley@outlook.com>
2024-08-09 08:47:19 -0500

    Commented out map code so it can run

--- client/src/app/components/BakeryMap/BakeryMap.tsx
+++ client/src/app/components/BakeryMap/BakeryMap.tsx
@@ -10,7 +10,7 @@ export default function BakeryMap() {
   };
   return (
     <div className={styles.BakeryMap}>
-      <MapContainer
+      {/* <MapContainer
         // center={[51.505, -0.09]}
         // zoom={13}
         style={{ height: '100%', width: '100%' }}
@@ -30,7 +30,7 @@ export default function BakeryMap() {
             </Popup>
           </Marker>
         ))}
-      </MapContainer>
+      </MapContainer> */}
     </div>
   );
 }
