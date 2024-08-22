e946aafe39b9a57b295dcd4eac7b8532a317c257
Taylor Kelley <taylorgkelley@outlook.com>
2024-08-22 18:38:45 -0500

    More tailwind for Navbar migrations

--- client/src/components/Navbar/Navbar.tsx
+++ client/src/components/Navbar/Navbar.tsx
@@ -12,8 +12,11 @@ export default function Navbar() {
     >
       <div className='fixed top-0 left-1/2 -translate-x-1/2 z-10 w-full'>
         <div className=' max-w-screen-2xl mx-auto bg-gray-800 flex justify-between py-4 px-12'>
-          <div className={styles.NavMain_findBakeryContainer}>
-            <Link href='/Search/Bakery'>
+          <div className='flex-1 flex items-center'>
+            <Link
+              href='/Search/Bakery'
+              className='flex items-center gap-2 relative z-10'
+            >
               <Location width='1rem' />
               <p>Find a Bakery</p>
             </Link>
@@ -21,7 +24,7 @@ export default function Navbar() {
           <div className={styles.NavMain_title}>
             <NavTitle />
           </div>
-          <div className={styles.NavMain_accountContainer}>
+          <div className='flex-1 flex items-center justify-end gap-8'>
             <Link href='/MyCart'>
               <Cart />
             </Link>
